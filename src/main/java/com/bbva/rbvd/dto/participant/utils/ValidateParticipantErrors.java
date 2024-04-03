bb8f852dc6ea71749bd6313533d36808f02bb2c9
paulkyev <paul.saire.contractor@bbva.com>
2024-04-03 09:43:44 -0500

    PARSRBVD-1955 PARSRBVD-2045 refactor: modofy code emuns adding BBVA code initial

--- src/main/java/com/bbva/rbvd/dto/participant/utils/ValidateParticipantErrors.java
+++ src/main/java/com/bbva/rbvd/dto/participant/utils/ValidateParticipantErrors.java
@@ -1,11 +1,11 @@
 package com.bbva.rbvd.dto.participant.utils;
 
 public enum ValidateParticipantErrors {
-    SELECT_DB_ORACLE_ERROR("RBVD00000132", false, "Error al ejecutar la consulta a la BD. "),
-    ERROR_BBVA_VALIDATION("RBVD10094946", false, "Error por validación interna de BBVA. "),
-    ERROR_INTERNAL_SERVICE_INVOKATION("RBVD10094947", false, "Error en la invocación de servicio BBVA. "),
-    TIMEOUT_ADD_PARTICIPANTS_RIMAC_ERROR("RBVD01020044", false, "Actualmente, el servicio validar participantes de Rimac no está disponible debido a un tiempo de espera en la conexión. Te recomendamos intentar acceder a este servicio en unos minutos, gracias."),
-    ERROR_NOT_FOUND("RBVD00000136", false, "No se encontró mensaje del servidor");
+    SELECT_DB_ORACLE_ERROR("BBVAERR00200000132", false, "Error al ejecutar la consulta a la BD. "),
+    ERROR_BBVA_VALIDATION("BBVAERR00210094946", false, "Error por validación interna de BBVA. "),
+    ERROR_INTERNAL_SERVICE_INVOKATION("BBVAERR00210094947", false, "Error en la invocación de servicio BBVA. "),
+    TIMEOUT_ADD_PARTICIPANTS_RIMAC_ERROR("BBVAERR00201020044", false, "Actualmente, el servicio validar participantes de Rimac no está disponible debido a un tiempo de espera en la conexión. Te recomendamos intentar acceder a este servicio en unos minutos, gracias."),
+    ERROR_NOT_FOUND("BBVAERR00200000136", false, "No se encontró mensaje del servidor");
 
     private final String adviceCode;
     private final boolean rollback;
